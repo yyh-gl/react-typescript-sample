d7bfb7f9ffaa591079d2ace8d97f7d4a9d64b093
yyh-gl <yhonda.95.gl@gmail.com>
2019-12-01 19:14:47 +0900

    ユーザ一覧表示処理を実装

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const List: React.FC = () => {
+import { fetchUsers } from '../api/user';
+import { User } from '../models/user';
+
+const Users: React.FC = () => {
+    const [userList, setUserList] = useState<User[] | undefined>(undefined);
+
+    const fetchUsersReq = async () => {
+        try {
+            const { data } = await fetchUsers();
+            return data;
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    useEffect(() => {
+        const data = fetchUsersReq();
+        data.then(users => {
+            setUserList(users);
+        });
+    }, []);
+
     return (
         <>
-            <h1>Profile</h1>
+            <h1>User List</h1>
+            {
+                userList && userList.map((user) => {
+                    return (
+                        <p key={user.id}>{`${user.lastName} ${user.firstName}`}</p>
+                    );
+                })
+            }
         </>
     );
 };
 
-export default List;
+export default Users;
